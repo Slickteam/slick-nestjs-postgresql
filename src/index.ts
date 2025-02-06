305c28b0b7d3f0f3ea72c4caa3ce5e6cfe9bf006
Ronan MOREL <ronan.morel@slickteam.fr>
2025-02-06 11:37:25 +0100

    fix: export lib

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-export * from 'typeorm';
+import { InjectDataSource, InjectEntityManager, InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
 
 import { databaseSettings } from './database-config';
+
+export * from 'typeorm';
 
 @Module({
   imports: [
@@ -14,4 +15,4 @@ import { databaseSettings } from './database-config';
 })
 class DatabaseModule {}
 
-export { DatabaseModule };
+export { DatabaseModule, InjectDataSource, InjectEntityManager, InjectRepository };
